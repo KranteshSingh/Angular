489a78d2e9c5bd20c6b33d1aa16668d24a931ce0
Krantesh Singh <34528823+KranteshSingh@users.noreply.github.com>
2019-09-15 11:42:33 +0530

    Angular

--- 02 Angular REST API/blog-frontend/src/app/app.module.ts
+++ 02 Angular REST API/blog-frontend/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { RouterModule, Routes } from '@angular/router';
 // Importing HttpClientModule in app.module.ts so that we can create http service in all the component.
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -28,8 +25,7 @@ import { BlogHttpService } from './blog-http.service';
     BlogCreateComponent,
     BlogEditComponent,
     AboutComponent,
-    NotFoundComponent,
-
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -39,8 +35,6 @@ import { BlogHttpService } from './blog-http.service';
     BrowserAnimationsModule, // required animations module
     ToastrModule.forRoot(),
     RouterModule.forRoot([
-
-
       { path: 'home', component: HomeComponent },
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'about', component: AboutComponent },
@@ -48,11 +42,9 @@ import { BlogHttpService } from './blog-http.service';
       { path: 'create', component: BlogCreateComponent },
       { path: 'edit/:blogId', component: BlogEditComponent },
       { path: '**', component: NotFoundComponent }
-
-
     ])
   ],
   providers: [BlogHttpService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
